75b659d48f44516659e2733164981dce2cc90bcb
Vojtěch Breník <vojta.brenik@outlook.cz>
2021-11-27 14:18:12 +0100

    added alerts and corrected returns

--- Server/api.py
+++ Server/api.py
@@ -62,8 +62,49 @@ class Api:
         try:
             self.measurements_api.create_measurement(self.team_uuid, measurement)
             self.log("D: Successfully sent measurement: " + msg)
+            return 1
         except openapi_client.ApiException as e:
             self.log("E: Exception when calling MeasurementsApi->create_measurement: " + str(e))
+            return 0
+
+    def send_alert(self, msg = None, temperature = None, created_on = None):
+        sensor_uuid = self.sensor.sensor_uuid
+        high_temperature = self.sensor.max_temperature
+        low_temperature = self.sensor.min_temperature
+
+        if not msg is None:
+            try:
+                measurement_dict = json.loads(msg)
+
+                created_on = measurement_dict["created_on"][0:-3]+"+00:00" # expecting UTC time
+                temperature = measurement_dict["temperature"]
+
+            except json.JSONDecodeError as err:
+                self.log("E: Message {0} is not a json.".format(msg))
+                self.log("   " + str(err))
+                return 0
+            except KeyError as err:
+                self.log("E: Missing measurement attribute in message: {0}.".format(msg))
+                self.log("   " + str(err))
+                return 0
+
+        if created_on is None:
+            created_on = datetime.datetime.now(datetime.timezone.utc).isoformat(timespec='milliseconds')
+            
+        if not temperature is None:
+            alert = Alert(created_on, sensor_uuid, temperature, high_temperature, low_temperature)
+        else:
+            self.log("E: Error when creating alert. Check supplied parameters.")
+            return 0
+
+        try:
+            # Store an alert
+            self.alerts_api.create_alert(self.team_uuid, alert)
+            return 1
+        except openapi_client.ApiException as e:
+            print("E: Exception when calling AlertsApi->create_alert: " + str(e))
+            return 0
+
 if __name__ == "__main__":
     api_client = Api("Orange", ">f@9C3p<")
     api_client.write_message('{"team_name": "white", "created_on": "2021-11-27T12:25:05.336974", "temperature": 25.72}')
