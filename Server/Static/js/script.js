f324e7715d00c551411bb133a5fcd08531b37509
Phidaimonia <tohanfreerun@gmail.com>
2021-12-05 17:04:58 +0100

    Initial sensor status commit

--- Server/Static/js/script.js
+++ Server/Static/js/script.js
@@ -69,9 +69,11 @@ function onSocketMessage(message) {
 
     if (data["response_type"] == "sensor_status")
     {
-        if("team_name" in data && "last_seen" in data)
+        if("last_seen" in data)
         {
-            //
+            lastSeenDate = new Date(data.lastSeen);
+            document.getElementById(data.team_name + 'Status').innerText = data.lastSeen;
+            //document.getElementById('aimtecOnlineElement').style.color = data["status"] ? "green" : "red"
         }
     }
 
