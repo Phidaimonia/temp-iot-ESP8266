9c226c32f11324fcd94e729587b0184edfd94b52
Phidaimonia <tohanfreerun@gmail.com>
2021-12-05 15:26:59 +0100

    Fix wrong method

--- Server/Static/js/script.js
+++ Server/Static/js/script.js
@@ -141,7 +141,7 @@ for(i = 0; i < chartCapacity; i++)
     //new_hr =  (startDate.getHours() + Math.floor((startDate.getMinutes() + i) / 60)) % 24
     //x_data[i] = new_hr.toString().padStart(2, "0") + ":" + new_min.toString().padStart(2, "0")
 
-    x_data[i] = new Date(endDate.now() + (i-chartCapacity) * timeframe )
+    x_data[i] = new Date(endDate.getTime() + (i-chartCapacity) * timeframe )
 }
 
 team_names.forEach((tm_name) => {                               // vytvori chart objekty
