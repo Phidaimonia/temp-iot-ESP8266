2e19a7b85af52bf7edd1f87f21eb58edee192921
Phidaimonia <tohanfreerun@gmail.com>
2021-12-05 14:41:32 +0100

    Fix typo

--- Server/Static/js/script.js
+++ Server/Static/js/script.js
@@ -44,10 +44,11 @@ function onSocketMessage(message) {
         }
    
         nowDate = new Date()
-        nowDate = nowDate.getTime() - nowDate.getSeconds() * 1000
-
         if(lastChartUpdateMin != nowDate.getMinutes())
             updateChart()
+            
+        nowDate = nowDate.getTime() - nowDate.getSeconds() * 1000
+
 
         measureDate = new Date(data.created_on)
         measureDate = measureDate.getTime() - measureDate.getSeconds() * 1000 + 30000 // posuneme o 30s dopredu
